b241869c72c898abf1a3c8b86fa16ffce9eb3429
junnan.zhao <junnan.zhao@reinventventures.com>
2021-11-01 00:47:54 +0800

    feat: add get request

--- src/main/java/com/simon/springbootk8s/controller/HelloController.java
+++ src/main/java/com/simon/springbootk8s/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.simon.springbootk8s.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
